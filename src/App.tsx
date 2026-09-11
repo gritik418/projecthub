b37e2c8d567b370595c078e5ba7835aa5ab7b83a
Ritik <160255700+gritik418@users.noreply.github.com>
2026-09-11 15:19:43 +0530

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router";
+import Login from "./pages/auth/Login";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
